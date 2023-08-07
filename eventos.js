90e1c1861fad83e52e230c901b5756abf3e85bec
le-janevo <j.fumeaux95@gmail.com>
2023-08-07 00:06:50 -0300

    agregar estilo y evento al clickear el div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let div = document.getElementById('divsito');
+
+    div.addEventListener('click', function () {
+        window.alert('Hola! Soy el Div')
+    });
+});
+
